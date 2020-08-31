99bb2eb57d20de2d6e31f10f70819b9b92323f6d
bdbose <57151177+bdbose@users.noreply.github.com>
2020-08-31 10:04:50 +0530

    Updates

---
+++ precache-manifest.080a25221b93a2cfaef0c017cb11b365.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ae93939bc525834d9594dd6c9ae58d3",
+    "url": "/Love/index.html"
+  },
+  {
+    "revision": "425520e3b6157d55aad3",
+    "url": "/Love/static/css/main.d4d9b167.chunk.css"
+  },
+  {
+    "revision": "4d65ae86be3dacd2e27e",
+    "url": "/Love/static/js/2.30ef94e5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Love/static/js/2.30ef94e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "425520e3b6157d55aad3",
+    "url": "/Love/static/js/main.3faeb15c.chunk.js"
+  },
+  {
+    "revision": "f301d02dd8a4d27c8271",
+    "url": "/Love/static/js/runtime-main.ecee6ab2.js"
+  },
+  {
+    "revision": "2064d0732fff10c77f436106c90aa2ad",
+    "url": "/Love/static/media/01_1400.2064d073.webp"
+  },
+  {
+    "revision": "a8a8e2d9ae26b0598bfbaeea0e787029",
+    "url": "/Love/static/media/09.a8a8e2d9.jpg"
+  },
+  {
+    "revision": "27abe67d9a6ad039166ef56b9da9dc16",
+    "url": "/Love/static/media/card.27abe67d.png"
+  },
+  {
+    "revision": "75a76f3e430d633618d602fbdd1935f6",
+    "url": "/Love/static/media/music.75a76f3e.mp3"
+  }
+]);
